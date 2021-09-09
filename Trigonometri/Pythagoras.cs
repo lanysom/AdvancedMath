923307b872001ea8910698f1a7a0870cd91f502a
Lars Nysom <LANY@ucn.dk>
2021-09-09 12:14:43 +0200

    Add project files.

---
+++ Trigonometri/Pythagoras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Trigonometri
+{
+    public class Pythagoras
+    {
+        public double CalculateLengthOfC(double a, double b)
+        {
+            // TODO: implement the formula that calculates the length of c
+            throw new NotImplementedException();
+        }
+
+        public double CalculateLengthOfB(double a, double c)
+        {
+            // TODO: implement the formula that calculates the length of b
+            throw new NotImplementedException();
+        }
+
+        public double CalculateLengthOfA(double b, double c)
+        {
+            // TODO: implement the formula that calculates the length of a
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
